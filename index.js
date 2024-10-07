c03f434546aa84ebcb6c7df71f0849a226d6bbc7
DeionSloc <dslocom@wgu.edu>
2024-10-06 22:20:29 -0600

    Updated index.html file by adding new classes for each job, and updated the index.js file to create button actions.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.ariaValue;
+function readMore(company) {
+    let dots = document.querySelector(`.card[data-company="${company}"] .dots`);
+    let moreText = document.querySelector(`.card[data-company="${company}"] .more`); 
+    let btnText = document.querySelector(`.card[data-company="${company}"] .myBtn`);
 
-slider.oninput = function() {
-    output.innerHTML = this.value;
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less"; 
+        moreText.style.display = "inline";
+    }
 }
